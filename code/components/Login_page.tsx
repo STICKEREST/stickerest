6ded646a423f14894ff992341ae191ad504665e2
Alice.Frezza <alice.frezza@stud-inf.unibz.it>
2022-11-08 16:57:21 +0100

    commenting css, organising code better

--- code/components/Login_page.tsx
+++ code/components/Login_page.tsx
@@ -8,9 +8,53 @@ import { styles } from "../style";
 
 import{ ImagesAssets } from '../assets/ImagesAssets';
 
-export default function Login_page() {
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+//this component renders the possibility to enter the mail and the password
+const TextFields = () => {
 
   const [text, onChangeText] = React.useState("Useless Text");
+
+  return (
+      <SafeAreaView>
+          <View style={[styles.input_container, {flexDirection:"column"}]}>
+              <View style={styles.inputs}>
+                  <Image style={styles.inputs_picture} source={ImagesAssets.mail}/>
+                  <TextInput
+                    style={[styles.input, {width: windowWidth*0.7}]}
+                    onChangeText={onChangeText}
+                    placeholder={"Email"}
+                  />
+              </View>
+              <View style={styles.inputs}>
+                  <Image style={[styles.inputs_picture, {width: 30, height: 30}]}  source={ImagesAssets.group}/>
+                  <TextInput
+                    style={[styles.input, {width: windowWidth*0.7}]}
+                    onChangeText={onChangeText}
+                    placeholder={"Password"}
+                  />
+              </View>
+          </View>
+      </SafeAreaView>
+  )
+}
+
+//this component renders the sign in button (not an actual button)
+const SignInButton = () => {
+  return (
+    <TouchableOpacity
+                    onPress={null}
+                    style={[styles.logInButton, {width: windowWidth/2}]}>
+                    <Text
+                      style={styles.logInButtonFont}>
+                      Sign In
+                    </Text>
+    </TouchableOpacity>
+  )
+}
+
+export default function Login_page() {
   
   const [fontsLoaded] = useFonts({
     'popblack': require('../assets/fonts/poppins/popblack.otf'),
@@ -22,44 +66,14 @@ export default function Login_page() {
     return null;
   }
 
-  const windowWidth = Dimensions.get('window').width;
-  const windowHeight = Dimensions.get('window').height;
-
   return (
     <View style={styles.container}>
         <ImageBackground source={ImagesAssets.bannerList2} style={{height: windowHeight, width: windowWidth}}>
             <View style={styles.text_view_login}>
                 <Text style={[styles.textLogin, {paddingTop: windowHeight/6}]}>Log in to your Account</Text>
-
-                <SafeAreaView>
-                   <View style={[styles.input_container, {flexDirection:"column"}]}>
-                        <View style={styles.inputs}>
-                            <Image style={styles.inputs_picture} source={ImagesAssets.mail}/>
-                            <TextInput
-                              style={[styles.input, {width: windowWidth*0.7}]}
-                              onChangeText={onChangeText}
-                              placeholder={"Email"}
-                            />
-                        </View>
-                        <View style={styles.inputs}>
-                            <Image style={[styles.inputs_picture, {width: 30, height: 30}]}  source={ImagesAssets.group}/>
-                            <TextInput
-                              style={[styles.input, {width: windowWidth*0.7}]}
-                              onChangeText={onChangeText}
-                              placeholder={"Password"}
-                            />
-                        </View>
-                    </View>
-                </SafeAreaView>
+                <TextFields/>
                 <View style={{marginTop: 20, alignSelf: 'center'}}>
-                  <TouchableOpacity
-                    onPress={null}
-                    style={[styles.logInButton, {width: windowWidth/2}]}>
-                    <Text
-                      style={styles.logInButtonFont}>
-                      Sign In
-                    </Text>
-                  </TouchableOpacity>
+                  <SignInButton/>
                 </View>
             </View>
         </ImageBackground>

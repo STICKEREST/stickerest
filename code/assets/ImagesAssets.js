5d902efe7dc74dc604e81d2aeacbe802e773b9f5
Francesco <francesco.mazzini432@gmail.com>
2022-11-16 07:21:22 +0100

    Added some components for the view of the single sticker
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- code/assets/ImagesAssets.js
+++ code/assets/ImagesAssets.js
@@ -1,5 +1,7 @@
 export const ImagesAssets = {
     bannerList1: require('./Sitckerest_png.png'),
     logo: require('./football.png'),
+    rectangleTop: require('./rectangleTop.png'),
+    computer: require('./computer.png')
     ///you can add more many images like this here.
 };

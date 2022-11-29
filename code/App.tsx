fc5e129a979f1e1e7aba704c2432adbe892dd190
Francesco <francesco.mazzini432@gmail.com>
2022-11-29 14:24:26 +0100

    Try
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- code/App.tsx
+++ code/App.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
 //FOR TESTING PURPOSES:
-//import SingleSticker from './components/pages/stickers/SingleSticker';
 import Loading_page from './components/pages/Loading_page';
-import Login_page from './components/pages/Login_page';
-
-import DicoveryPage from './components/pages/discovery/DiscoveryPage';
+// import Login_page from './components/pages/Login_page';
+// import Registration_page from './components/pages/Registration_page';
+// import SingleSticker from './components/pages/stickers/SingleSticker';
+// import DicoveryPage from './components/pages/discovery/DiscoveryPage';
 
 export default function App() {
 	return (
-      <View><Login_page/></View>
+      <View><Loading_page/></View>
+	//   <View><Login_page/></View>
+	//   <View><Registration_page/></View>
+	//   <View><SingleSticker/></View>
+	//   <View><DicoveryPage/></View>
+
 	);
 }

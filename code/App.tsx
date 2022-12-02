0ae6a32eece7bf99a8f5527f830165b1a4b2c663
Nicholas Amigoni <NAmigoni@unibz.it>
2022-12-02 14:34:59 +0100

    User profile page and discovery page are now accessible through the tab navigator

--- code/App.tsx
+++ code/App.tsx
@@ -3,14 +3,11 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import TabNavigator from './components/TabNavigator';
-import Registration_page from './components/pages/Registration_page';
-import Loading_page from './components/pages/Loading_page';
-import Login_page from './components/pages/Login_page';
-import User_page from './components/pages/UserProfilePage';
-
 
 export default function App() {
 	return (
-		<View><User_page/></View>
+		<NavigationContainer>
+			<TabNavigator />
+		</NavigationContainer>
 	);
 }
--- code/components/TabNavigator.tsx
+++ code/components/TabNavigator.tsx
@@ -4,22 +4,24 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { ImagesAssets } from '../assets/ImagesAssets';
 
 import Homepage from './pages/Homepage';
+import DiscoveryPage from './pages/discovery/DiscoveryPage';
+import UserProfilePage from './pages/UserProfilePage';
 
 import Navbar from './Navbar';
 
 const Tabs = createBottomTabNavigator();
 
 // TabNavigator component needs to be inside a NavigationContainer tag and it wraps the whole app.
-// The 'component' parameter for each screen is the component rendered on that screen.
-// 'initialParams' is used to pass params to the navigation route.
 export default function TabNavigator() {
+	// The 'component' parameter for each screen is the component rendered on that screen.
+	// 'initialParams' is used to pass params to the navigation route.
 	return (
 		<Tabs.Navigator screenOptions={{headerShown: false}} tabBar={Navbar} >
 			<Tabs.Screen name="Home" component={Homepage} initialParams={{icon: ImagesAssets.iconHome}} />
-			<Tabs.Screen name="Favourites" component={View} initialParams={{icon: ImagesAssets.iconStar}} />
+			<Tabs.Screen name="Discovery" component={DiscoveryPage} initialParams={{icon: ImagesAssets.iconStar}} />
 			<Tabs.Screen name="Add" component={View} initialParams={{icon: ImagesAssets.iconDocument}} />
 			<Tabs.Screen name="Settings" component={View} initialParams={{icon: ImagesAssets.iconSettings}} />
-			<Tabs.Screen name="User" component={View} initialParams={{icon: ImagesAssets.iconUser}} />
+			<Tabs.Screen name="User" component={UserProfilePage} initialParams={{icon: ImagesAssets.iconUser}} />
 		</Tabs.Navigator>
 	);
 }

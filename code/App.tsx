b9d40cc99b869d2b848d048592a9415a107a7ea8
Siferraris <siferraris@unibz.it>
2022-12-05 21:26:23 +0100

    merge fixes

--- code/App.tsx
+++ code/App.tsx
@@ -1,27 +1,14 @@
-import React from 'react'
-import { StatusBar } from 'expo-status-bar';
-import { Dimensions } from 'react-native';
-import { StyleSheet, Text, View, Image } from 'react-native';
 
 import React from 'react'
 import { Dimensions, ImageBackground } from 'react-native';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
-import Loading_page from './components/Loading_page';
-
 import { useFonts } from 'expo-font';
 
 import{ ImagesAssets } from './assets/ImagesAssets';
 
-import SingleSticker from './components/pages/stickers/SingleSticker';
-
 import CreatePack from './components/pages/creationPages/createPack';
 
-import BigStickerCarousel from './components/subcomponents/stickers-carousel/CarouselBigSticker';
-
-import StickerCarousel from './components/subcomponents/stickers-carousel/CarouselSticker';
-
-import HomePage from './components/pages/homepage/Homepage';
 
             //                    code\components\Loading_page.tsx
             //                    ./components/pages/homepage/Homepage
@@ -29,25 +16,10 @@ import HomePage from './components/pages/homepage/Homepage';
 
 import TabNavigator from './components/subcomponents/TabNavigator';
 
-import Carousel from './components/subcomponents/stickers-carousel/CarouselBigSticker';
+import { TagInput } from './components/subcomponents/tags-input/TagInput';
 
-/*
-  import React from 'react'
-
-  import { NavigationContainer } from '@react-navigation/native';
-  import TabNavigator from './components/TabNavigator';
-
-  export default function App() {
-    return (
-      <NavigationContainer>
-        <TabNavigator />
-      </NavigationContainer>
-    );
-  }
-*/
 
 export default function App() {
-
   return (
     <View>
       <CreatePack/>

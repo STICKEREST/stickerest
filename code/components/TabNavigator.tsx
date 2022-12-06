8a4e152b134d3b4907387379222d737b5f3cf0dd
Nicholas Amigoni <NAmigoni@unibz.it>
2022-12-06 17:11:32 +0100

    Moved Navbar into TabNavigator because it is only needed there

--- code/components/TabNavigator.tsx
+++ code/components/TabNavigator.tsx
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Image } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import { ImagesAssets } from '../assets/ImagesAssets';
@@ -7,8 +7,6 @@ import Homepage from './pages/Homepage';
 import DiscoveryPage from './pages/discovery/DiscoveryPage';
 import UserProfilePage from './pages/UserProfilePage';
 import CreatePack from './pages/creationPages/createPack';
-
-import Navbar from './Navbar';
 
 const Tabs = createBottomTabNavigator();
 
@@ -26,3 +24,64 @@ export default function TabNavigator() {
 		</Tabs.Navigator>
 	);
 }
+
+function Navbar({state, descriptors, navigation}) {
+	return (
+		<View style={navbarStyles.background}>
+			{
+				state.routes.map((route, index) => {
+					const buttons = state.routes.length;
+					const middleButton = (buttons % 2 === 0 ? buttons : buttons - 1) / 2;
+					const navigate = () => {
+						navigation.navigate({
+							name: route.name,
+							merge: true
+						});
+					}
+					return index === middleButton ? (
+						<View style={navbarStyles.circleButton} key={index}>
+							<TouchableOpacity onPress={navigate}>
+								<Image source={route.params.icon} style={{alignSelf: "center"}} />
+							</TouchableOpacity>
+						</View>
+					) : (
+						<TouchableOpacity style={navbarStyles.expand} onPress={navigate} key={index}>
+							<Image source={route.params.icon} style={{alignSelf: "center"}} />
+						</TouchableOpacity>
+					);
+				})
+			}
+		</View>
+	);
+}
+
+const navbarStyles = StyleSheet.create({
+	background: {
+		position: "absolute",
+		bottom: 0,
+		flexDirection: "row",
+		backgroundColor: "#f5cb08",
+		paddingBottom: 15,
+		paddingTop: 10,
+		borderTopLeftRadius: 20,
+		borderTopRightRadius: 20
+	},
+	expand: {
+		flexBasis: "auto",
+		flexGrow: 1,
+		flexShrink: 0
+	},
+	circleButton: {
+		backgroundColor: "#f5cb08",
+		paddingTop: 15,
+		marginTop: -60,
+		height: 90,
+		width: 90,
+		borderRadius: 90,
+		shadowOffset: {
+			width: 0,
+			height: 2
+		},
+		elevation: 5
+	}
+});

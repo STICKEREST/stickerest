d97ee18e21b2accc8637fee07830eb499712344e
Nicholas Amigoni <NAmigoni@unibz.it>
2022-12-07 11:25:17 +0100

    App now shows loading screen for 5 seconds

--- code/App.tsx
+++ code/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import TabNavigator from './components/TabNavigator';
 
+import Loading_page from './components/pages/Loading_page';
 
 export default function App() {
-	return (
+	const [ready, setReady] = useState(false);
+	useEffect(() => {
+		async function load() {
+			try {
+				// TODO: This waits for 5 seconds, the loading should be done here
+				await new Promise(resolve => setTimeout(resolve, 5000));
+			} catch(e) {
+				console.warn(e);
+			} finally {
+				setReady(true);
+			}
+		}
+		load();
+	});
+	// TODO: Use more than just one 'ready' state to show either Loading, Login, or TabNavigator
+	return ready ? (
 		<NavigationContainer>
 			<TabNavigator />
 		</NavigationContainer>
-	);
+	) : <Loading_page />;
 }

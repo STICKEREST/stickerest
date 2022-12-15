6e2ca24b4018cc85bb061b62e8df4fdbf8753e9b
Nicholas Amigoni <NAmigoni@unibz.it>
2022-12-15 15:33:58 +0100

    Created Telegram bot to use Telegram API

--- code/components/Test.tsx
+++ code/components/Test.tsx
@@ -1,8 +1,11 @@
 import React, { useCallback } from "react";
 import { Alert, Button, Linking, View } from "react-native";
 
-//const API_TOKEN = '';
+// TODO: I tried to make this work using .env but kept getting 'undefined'
+import { TelegramApiKey } from "../api_keys";
 
+// Button that can add an existing sticker pack to your telegram given the name of the pack. See examples below.
+// This does not use Telegram API, but needs the pack to be uploaded on telegram.
 const AddStickersButton = ({packName}: {packName: string}) => {
 	const url = 'https://telegram.me/addstickers/' + packName;
 	const onPress = useCallback(async() => {
@@ -18,20 +21,27 @@ const AddStickersButton = ({packName}: {packName: string}) => {
 	);
 }
 
-/*
+// Uses the telegram api to get information about the bot
+// TODO: Import stickers with https://core.telegram.org/api/stickers
 const fetchAPI = () => {
-	return fetch('https://api.telegram.org/botTOKEN/getMe').then((response) => response.json()).then((json) => {
+	return fetch('https://api.telegram.org/bot' + TelegramApiKey + '/getMe').then((response) => response.json()).then((json) => {
 		console.log(json)
 	}).catch((error) => {
 		console.error(error);
 	});
-};*/
+};
 
+// This is just a test
 export const Test = () => (
-	<View style={{paddingTop: 300}}>
-		<AddStickersButton packName="NekoHD" />
-		<AddStickersButton packName="Kiritsu" />
-		<AddStickersButton packName="Kurolily" />
-		<AddStickersButton packName="thehuskies" />
+	<View>
+		<View style={{paddingTop: 100}}>
+			<AddStickersButton packName="NekoHD" />
+			<AddStickersButton packName="Kiritsu" />
+			<AddStickersButton packName="Kurolily" />
+			<AddStickersButton packName="thehuskies" />
+		</View>
+		<View style={{paddingTop: 100}}>
+			<Button title="Use API" onPress={fetchAPI} />
+		</View>
 	</View>
 );

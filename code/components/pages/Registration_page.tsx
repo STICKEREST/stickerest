ab7b382ea782bb58f59c8703da1b50af7310ed4d
Alice.Frezza <alice.frezza@stud-inf.unibz.it>
2022-12-18 11:22:57 +0100

    alert message in case field are missing

--- code/components/pages/Registration_page.tsx
+++ code/components/pages/Registration_page.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import { Dimensions, ImageBackground, Linking, SafeAreaView, TextInput} from 'react-native';
-import { Text, View, Image, Button, TouchableOpacity, Pressable } from 'react-native';
+import { Text, View, Image, Button, TouchableOpacity, Pressable, Alert } from 'react-native';
 
 import { useFonts } from 'expo-font';
 
@@ -28,20 +28,60 @@ const TextFields = ({email, password, setEmail, setPassword, nickname, setNickna
   )
 }
 
-//this component renders the sign up button (not an actual button)
-const SignUpButton = ({onSubmit} : {onSubmit : any}) => {
-  return (
-    <TouchableOpacity
-                    onPress={onSubmit}
-                    style={[styles.logInButton, {width: windowWidth/2}]}>
-                    <Text
-                      style={styles.logInButtonFont}>
-                      Sign Up
-                    </Text>
-    </TouchableOpacity>
-  )
+function isEmpty(stringValue:string): boolean
+{
+  return stringValue==null || stringValue.trim()===""
 }
 
+const handleSignUp = (emailSign : string, nickSign :string, pwSign : string) => {
+
+
+  let formBody = [];
+
+  console.log("email: " + emailSign)
+
+  if (isEmpty(emailSign) || isEmpty(nickSign) || isEmpty(pwSign))
+  {
+    Alert.alert(
+      "Info missing",
+      "Email address and/or password are missing, please enter them",
+      [
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ]
+    );
+  } else {
+      formBody.push(encodeURIComponent("email") + "=" + encodeURIComponent(emailSign));
+    
+      formBody.push(encodeURIComponent("nickname") + "=" + encodeURIComponent(nickSign));
+      
+      formBody.push(encodeURIComponent("password") + "=" + encodeURIComponent(pwSign));
+    
+      console.log(formBody)
+      
+    
+      //@ts-ignore
+      formBody = formBody.join("&");    
+    
+      fetch("https://stickerest.herokuapp.com/users/register", {
+        method: 'POST',
+        //@ts-ignore
+        body: formBody,//post body 
+        headers: {//Header Defination 
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+      } )
+      .then((response) => {
+        if(response.status === 201)
+          console.log('Successful registration');
+        else
+          console.log('Something went wrong during the registration');
+      })
+      .catch((error) => {
+        console.log('Something went wrong during the registration');
+      });
+  }
+
+}
 
 
 export default function Registration_page() {
@@ -50,41 +90,23 @@ export default function Registration_page() {
   const [nickname, setNickname] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
-  const handleSignUp = (emailSign : string, nickSign :string, pwSign : string) => {
+  //this component renders the sign up button (not an actual button)
+  const SignUpButton = ({onSubmit} : {onSubmit : any}) => {
+    return (
+      <TouchableOpacity
+                      onPress={startHandlingData}
+                      style={[styles.logInButton, {width: windowWidth/2}]}>
+                      <Text
+                        style={styles.logInButtonFont}>
+                        Sign Up
+                      </Text>
+      </TouchableOpacity>
+    )
+  }
 
-
-    let formBody = [];
-  
-    formBody.push(encodeURIComponent("email") + "=" + encodeURIComponent(emailSign));
-    
-    formBody.push(encodeURIComponent("nickname") + "=" + encodeURIComponent(nickSign));
-    
-    formBody.push(encodeURIComponent("password") + "=" + encodeURIComponent(pwSign));
-
-    console.log(formBody)
-    
-  
-    //@ts-ignore
-    formBody = formBody.join("&");    
-  
-    fetch("https://stickerest.herokuapp.com/users/register", {
-      method: 'POST',
-      //@ts-ignore
-      body: formBody,//post body 
-      headers: {//Header Defination 
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-    } )
-    .then((response) => {
-      if(response.status === 201)
-        console.log('Successful registration');
-      else
-        console.log('Something went wrong during the registration');
-    })
-    .catch((error) => {
-      console.log('Something went wrong during the registration');
-    });
-  
+  function startHandlingData()
+  {
+    handleSignUp(email, password, nickname)
   }
   
   const [fontsLoaded] = useFonts({

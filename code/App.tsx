720aef6803d4ceff6aebdb926b770b91b754f1c4
Francesco <francesco.mazzini432@gmail.com>
2022-12-29 11:51:26 +0100

    Finished link but it has to be tested
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- code/App.tsx
+++ code/App.tsx
@@ -11,40 +11,36 @@ import Registration_page from './components/pages/Registration_page';
 import {SingleSticker} from './components/pages/stickers/SingleSticker';
 import UserProfilePage from './components/pages/UserProfilePage';
 import CreatePack from './components/pages/creationPages/createPack';
+import DiscoveryPage from './components/pages/discovery/DiscoveryPage';
 
 export default function App() {
 	
-	// const [ready, setReady] = useState(false);
-	// const [loggedIn, setLoggedIn] = useState(false); // TODO: Simulates the user being logged in or not, change this when actual login is implemented
+	const [ready, setReady] = useState(false);
+	const [loggedIn, setLoggedIn] = useState(false); // TODO: Simulates the user being logged in or not, change this when actual login is implemented
 
 	
-	// useEffect(() => {
-	// 	async function load() {
-	// 		try {
-	// 			// TODO: This waits for 5 seconds, the loading should be done here
-	// 			await new Promise(resolve => setTimeout(resolve, 5000));
-	// 		} catch(e) {
-	// 			console.warn(e);
-	// 		} finally {
-	// 			setReady(true);
-	// 		}
-	// 	}
-	// 	load();
-	// });
-	// return ready ? loggedIn ? (
-	// 	<NavigationContainer>
-	// 		<StackNavigator />
-	// 	</NavigationContainer>
-	// ) : (
-	// 	<Login_page />
-	// ) : (
-	// 	<Loading_page />
-	// );
+	useEffect(() => {
+		async function load() {
+			try {
+				// TODO: This waits for 5 seconds, the loading should be done here
+				await new Promise(resolve => setTimeout(resolve, 5000));
+			} catch(e) {
+				console.warn(e);
+			} finally {
+				setReady(true);
+			}
+		}
+		load();
+	});
+	return ready ? loggedIn ? (
+		<NavigationContainer>
+			<StackNavigator />
+		</NavigationContainer>
+	) : (
+		<Login_page />
+	) : (
+		<Loading_page />
+	);
 
-	return (
-		<View>
-			<CreatePack />
-		</View>
-	)
 		
 }

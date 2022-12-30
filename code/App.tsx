edd7945061eae494ab7ba8dfb02dd7e7934460dd
Francesco <francesco.mazzini432@gmail.com>
2022-12-30 13:00:17 +0100

    Done backend linking with userView but it has to be adjusted
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- code/App.tsx
+++ code/App.tsx
@@ -15,32 +15,37 @@ import DiscoveryPage from './components/pages/discovery/DiscoveryPage';
 
 export default function App() {
 	
-	const [ready, setReady] = useState(false);
-	const [loggedIn, setLoggedIn] = useState(false); // TODO: Simulates the user being logged in or not, change this when actual login is implemented
+	// const [ready, setReady] = useState(false);
+	// const [loggedIn, setLoggedIn] = useState(false); // TODO: Simulates the user being logged in or not, change this when actual login is implemented
 
 	
-	useEffect(() => {
-		async function load() {
-			try {
-				// TODO: This waits for 5 seconds, the loading should be done here
-				await new Promise(resolve => setTimeout(resolve, 5000));
-			} catch(e) {
-				console.warn(e);
-			} finally {
-				setReady(true);
-			}
-		}
-		load();
-	});
-	return ready ? loggedIn ? (
-		<NavigationContainer>
-			<StackNavigator />
-		</NavigationContainer>
-	) : (
-		<Login_page />
-	) : (
-		<Loading_page />
-	);
+	// useEffect(() => {
+	// 	async function load() {
+	// 		try {
+	// 			// TODO: This waits for 5 seconds, the loading should be done here
+	// 			await new Promise(resolve => setTimeout(resolve, 5000));
+	// 		} catch(e) {
+	// 			console.warn(e);
+	// 		} finally {
+	// 			setReady(true);
+	// 		}
+	// 	}
+	// 	load();
+	// });
+	// return ready ? loggedIn ? (
+	// 	<NavigationContainer>
+	// 		<StackNavigator />
+	// 	</NavigationContainer>
+	// ) : (
+	// 	<Login_page />
+	// ) : (
+	// 	<Loading_page />
+	// );
 
+	return (
+		<View>
+			<UserProfilePage />
+		</View>
+	)
 		
 }
--- code/components/pages/UserProfilePage.tsx
+++ code/components/pages/UserProfilePage.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import { Dimensions, ImageBackground, Linking, SafeAreaView, TextInput} from 'react-native';
 import { Text, View, Image, Button, TouchableOpacity, Pressable } from 'react-native';
 
@@ -13,19 +13,34 @@ import ButtonToSign from "./../subcomponents/ButtonToSign"
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
-import FieldComponent from "./../subcomponents/Field"
+import FieldComponent from "./../subcomponents/Field";
 
-const TextFields = () => {
+const handleUpdateUser = (nickname : string) => {
 
-    const [name, setName] = React.useState("name surname");
-    const [email, setEmail] = React.useState("name.surname@gmail.com");    
-    const [nickname, setNickname] = React.useState("nickname");
+}
+
+const TextFields = ({email, setEmail, nickname, setNickname} : {email : string, setEmail : any, nickname : string, setNickname : any}) => {
+
+    // const [name, setName] = React.useState("name surname");
+    // const [email, setEmail] = React.useState("name.surname@gmail.com");    
+    // const [nickname, setNickname] = React.useState("nickname");
+
+    useEffect(() => {
+
+      fetch("https://stickerest.herokuapp.com/auth/me")
+      .then(response => response.json())
+      .then(result => {
+        setNickname(result[0].nickname);
+        setEmail(result[0].email);
+      })
+
+    }, [])
 
     return (
         <SafeAreaView>
             <View style={styles.input_container}>
-                <FieldComponent name={name} placeholder={null} setName={setName} picture={"man-outline"} hide={false}/>
-                <FieldComponent name={nickname} placeholder={null} setName={setNickname} picture={"person-outline"} hide={false}/>
+                {/* <FieldComponent name={name} placeholder={null} setName={setName} picture={"man-outline"} hide={false}/> */}
+                <FieldComponent name={nickname} placeholder={null}   setName={setNickname} picture={"person-outline"} hide={false}/>
                 <FieldComponent name={email} placeholder={null} setName={setEmail} picture={"mail-outline"} hide={false}/>
             </View>
         </SafeAreaView>
@@ -33,6 +48,9 @@ const TextFields = () => {
 } 
 
 export default function UserProfilePage() {
+
+  const [email, setEmail] = React.useState("name.surname@gmail.com");    
+    const [nickname, setNickname] = React.useState("nickname");
   
   const [fontsLoaded] = useFonts({
     'popblack': require('./../../assets/fonts/poppins/popblack.otf'),
@@ -49,9 +67,9 @@ export default function UserProfilePage() {
           <ImageBackground source={ImagesAssets.bannerList2} resizeMode="stretch" style={{width: windowWidth, height: windowHeight}}>
             <View style={{alignItems:'center'}}><Text style={[styles.textUserProfile, {paddingTop: windowHeight/6}]}>Your Profile</Text></View>
               <View style={[styles.text_view_login, {marginTop: windowHeight*0.3}]}>
-                  <TextFields/>
+                  <TextFields email={email} setEmail={setEmail} nickname={nickname} setNickname={setNickname} />
                   <View style={[styles.style_signInButton, {marginTop: windowHeight*0.07}]}>
-                    <ButtonToSign functionToExecute={() => {}} nameOfButton="Save"/>
+                    <ButtonToSign functionToExecute={() => handleUpdateUser(nickname)} nameOfButton="Save"/>
                   </View>
               </View>
           </ImageBackground>

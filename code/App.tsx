d9ddd7fe608615ff350a4212266a47bdf3bc5802
Nicholas Amigoni <NAmigoni@unibz.it>
2023-01-11 20:53:20 +0100

    Loading is now async

--- code/App.tsx
+++ code/App.tsx
@@ -5,15 +5,30 @@ import { StackNavigator } from './components/StackNavigator';
 
 import Loading_page from './components/pages/Loading_page';
 
-import { useFonts } from 'expo-font';
+import * as Font from 'expo-font';
 
 export default function App() {
-	const [fontsLoaded] = useFonts({
-		'popblack': require('./assets/fonts/poppins/popblack.otf'),
-		'poplight': require('./assets/fonts/poppins/Poppins-Light.otf'),
-		'popregular': require('./assets/fonts/poppins/Poppins-Regular.otf')
+	const [ready, setReady] = useState(false);
+	useEffect(() => {
+		async function load() {
+			try {
+				// Async loading of fonts
+				await Font.loadAsync({
+					'popblack': require('./assets/fonts/poppins/popblack.otf'),
+					'poplight': require('./assets/fonts/poppins/Poppins-Light.otf'),
+					'popregular': require('./assets/fonts/poppins/Poppins-Regular.otf')
+				});
+				// Keep loading page visible for at least 1 second
+				await new Promise(resolve => setTimeout(resolve, 1000));
+			} catch(e) {
+				console.warn(e);
+			} finally {
+				setReady(true);
+			}
+		}
+		load();
 	});
-	return fontsLoaded ? (
+	return ready ? (
 		<NavigationContainer>
 			<StackNavigator />
 		</NavigationContainer>
--- code/components/StackNavigator.tsx
+++ code/components/StackNavigator.tsx
@@ -3,7 +3,6 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { TabNavigator } from './TabNavigator';
 import { SingleSticker } from './pages/stickers/SingleSticker';
 
-import Loading_page from './pages/Loading_page';
 import Login_page from './pages/Login_page';
 import Registration_page from './pages/Registration_page';
 
@@ -12,7 +11,6 @@ const Stack = createStackNavigator();
 
 export const StackNavigator = () => (
 	<Stack.Navigator screenOptions={{headerTransparent: true, headerTitle: '', headerTintColor: 'white'}} >
-		{/*<Stack.Screen name="LoadingPage" component={Loading_page} />*/}
 		<Stack.Screen name="LoginPage" component={Login_page} />
 		<Stack.Screen name="SignInPage" component={Registration_page} />
 		<Stack.Screen name="TabNavigator" component={TabNavigator} />

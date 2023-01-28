529903af4ca9e8b3341822dfa994e9d595a8cdda
Siferraris <siferraris@unibz.it>
2023-01-28 15:55:20 +0100

    refactoring taginput

--- code/components/TagInput.tsx
+++ code/components/TagInput.tsx
@@ -1,79 +1,47 @@
-import React from 'react';
-import { TextInput, TouchableOpacity, Text, View  } from 'react-native';
+import React, { Dispatch, SetStateAction } from 'react';
+import { TextInput, TouchableOpacity, Text, View, NativeSyntheticEvent, TextInputChangeEventData, TextInputKeyPressEventData  } from 'react-native';
 
 import { tagInputStyle } from '../styles/TagInput';
 import { styles } from '../styles/Styles';
 
 
-const TagInput = ({tags, setTags}:{tags:any, setTags: any}) => {
+const allowedTagsNmbr = 9;
 
-    const allowedTagsNmbr = 9;
-    const [isEditable, setEdit] = React.useState(true)
-    
-    const [input, setInput] = React.useState('');
-
-    function handleTagsNmb (n:any) {
-      if(n === allowedTagsNmbr-1) {
+const TagsContainer = ({tags, setTags, setEdit}:{tags:string[], setTags: Dispatch<SetStateAction<string[]>>, setEdit: Dispatch<SetStateAction<boolean>>}) => {
+    const deleteTag = (index : number) => {
+      setTags((prevState : string[]) => prevState.filter((tag : string, i : number) => i !== index))
+      if(tags.length === allowedTagsNmbr-1) {
         setEdit(false);
       } else {
         setEdit(true);
       }
-    }
+  };
 
-    const handleOnChangeText = (e:any) => {
-    
-      const text = e.nativeEvent.text;
-      setInput(text);
-      if(text.includes(',') || text.includes('Enter') || text.includes(' ')) {
-        e.preventDefault();
-        const trimmedInput = input.replace(/[|&;$%@"'<>()+,]/g, "");
-        if(trimmedInput != '' && !tags.includes(trimmedInput)) {
-          setTags((prevState : any) => [...prevState, trimmedInput]);
-        }
-        setInput('');
-        handleTagsNmb(tags.length);
-      }
-    };
-
-    const handleOnKeyDown = (e : any) => {
-        const {key} = e.nativeEvent;
-
-        if (key === "Backspace" && !input.length && tags.length) {
-          e.preventDefault();
-          const tagsCopy = [...tags];
-          const poppedTag = tagsCopy.pop();
-      
-          setTags(tagsCopy);
-          setInput(poppedTag);
-        }
-    };
-
-    const deleteTag = (index : any) => {
-        setTags((prevState : any) => prevState.filter((tag : any, i : any) => i !== index))
-        handleTagsNmb(tags.length);
-    };
   return (
-    <View style={styles.margin}>
-      <TextInput value={input} placeholder={isEditable ? "Press space or add a comma after each tag" : " "} onKeyPress={handleOnKeyDown} onChange={handleOnChangeText} editable={isEditable} />
-      <View style={tagInputStyle.tagsList}>
-      {
-        tags.map((tag: any, index: any) => (
-            <View key={tag} style={[tagInputStyle.tagContainer, {width: tag.length > 3 ? tag.length * 13 : 4 * 13}]}>
-              <View style={tagInputStyle.tagFlex}>
-                <View style={{flex: tag.length}}>
-                  <Text style={tagInputStyle.tagText}>{tag}</Text>
-                </View>
-                <View style={styles.flexFill}>
-                  <TouchableOpacity onPress ={() => deleteTag(index)}>
-                    <Text style={tagInputStyle.tagText}>x</Text>
-                  </TouchableOpacity>
+    <View style={tagInputStyle.tagsList}>
+        {
+          tags.map((tag: string, index: number) => (
+              <View key={tag} style={[tagInputStyle.tagContainer, {width: tag.length > 3 ? tag.length * 13 : 4 * 13}]}>
+                <View style={tagInputStyle.tagFlex}>
+                  <View style={{flex: tag.length}}>
+                    <Text style={tagInputStyle.tagText}>{tag}</Text>
+                  </View>
+                  <View style={styles.flexFill}>
+                    <TouchableOpacity onPress ={() => deleteTag(index)}>
+                      <Text style={tagInputStyle.tagText}>x</Text>
+                    </TouchableOpacity>
+                  </View>
                 </View>
               </View>
-            </View>
-        ))
-      }
+          ))
+        }
       </View>
-      <View style={tagInputStyle.tagsCount}>
+  )
+}
+
+const BottomTagsContainer = ({tags, setTags, setEdit}:{tags:string[], setTags: Dispatch<SetStateAction<string[]>>, setEdit: Dispatch<SetStateAction<boolean>>}) => {
+  return (
+  <View style={tagInputStyle.tagsCount}>
         <View>
           <Text>{allowedTagsNmbr-tags.length} tags available</Text>
         </View>
@@ -90,6 +58,73 @@ const TagInput = ({tags, setTags}:{tags:any, setTags: any}) => {
         }
         </View>
       </View>
+  )
+}
+
+
+const TagInput = ({tags, setTags}:{tags:string[], setTags: Dispatch<SetStateAction<string[]>>}) => {
+
+    const [isEditable, setEdit] = React.useState(true)
+    
+    const [input, setInput] = React.useState('');
+
+    /**
+     * Changes the edibility when the maximal number of tags is exceeded 
+     */
+    function handleTagsNmb () {
+      if(tags.length === allowedTagsNmbr-1) {
+        setEdit(false);
+      } else {
+        setEdit(true);
+      }
+    }
+
+    /**
+     * If the user presses ',' or 'Enter' or 'Space', trim and add to the tags array the input text.
+     * Then clear the input text and call handleTagsNmb() 
+     */
+    const handleOnChangeText = (e:NativeSyntheticEvent<TextInputChangeEventData>) => {
+    
+      const text = e.nativeEvent.text;
+      setInput(text);
+      if(text.includes(',') || text.includes('Enter') || text.includes(' ')) {
+        e.preventDefault();
+        const trimmedInput = input.replace(/[|&;$%@"'<>()+,.]/g, "");
+        if(trimmedInput != '' && !tags.includes(trimmedInput)) {
+          setTags((prevState : string[]) => [...prevState, trimmedInput]);
+        }
+        setInput('');
+        handleTagsNmb();
+      }
+    };
+
+    /**
+     * Pop the last tag added to the tags array when 'Backspace' is pressed.
+     * Set the input to the popped tag
+     * @param e Key pressed
+     */
+    const handleOnKeyDown = (e : NativeSyntheticEvent<TextInputKeyPressEventData>) => {
+        const {key} = e.nativeEvent;
+
+        if (key === "Backspace" && !input.length && tags.length) {
+          e.preventDefault();
+          const tagsCopy = [...tags];
+          const poppedTag = tagsCopy.pop();
+      
+          setTags(tagsCopy);
+          setInput(poppedTag);
+        }
+    };
+   
+  return (
+    <View style={styles.margin}>
+      <TextInput value={input} placeholder={isEditable ? "Press space or add a comma after each tag" : " "} onKeyPress={handleOnKeyDown} onChange={handleOnChangeText} editable={isEditable} />
+      
+      <TagsContainer tags={tags} setTags={setTags} setEdit={setEdit} />
+      <BottomTagsContainer tags={tags} setTags={setTags} setEdit={setEdit} />
+
+      
+
     </View>
   );
 }
